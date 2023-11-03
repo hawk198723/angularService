832b220bd47f0dff9f9e209a832f44bcbd469f5b
hawk198723 <hawk198723@gmail.com>
2023-11-03 16:57:09 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TeamsComponent } from './teams.component';
+import { TeamService } from './team.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TeamsComponent],
+  imports: [BrowserModule],
+  providers: [TeamService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
